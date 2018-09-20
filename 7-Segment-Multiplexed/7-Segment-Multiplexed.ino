f0dbbf8f515912b62ae31164035104aa89f3a38f
Gyrotron <simmarkalsi@gmail.com>
2018-09-21 01:17:11 +0530

    added two's compliment decoding

--- 7-Segment-Multiplexed/7-Segment-Multiplexed.ino
+++ 7-Segment-Multiplexed/7-Segment-Multiplexed.ino
@@ -81,21 +81,44 @@ void setup() {
   byte digits[] = { 0x7e, 0x30, 0x6d, 0x79, 0x33, 0x5b, 0x5f, 0x70, 0x7f, 0x7b }; //0-9
 
   Serial.println("Programming ones place");
-  for (int value = 0; value < 255; value += 1) {
+  for (int value = 0; value <= 255; value += 1) {
     writeEEPROM(value, digits[value % 10]); //one's place
   }
   Serial.println("Programming tens place");
-  for (int value = 0; value < 255; value += 1) {
+  for (int value = 0; value <= 255; value += 1) {
     writeEEPROM(value + 256, digits[(value / 10) % 10]); //ten's place
   }
   Serial.println("Programming hundreds place");
-  for (int value = 0; value < 255; value += 1) {
+  for (int value = 0; value <= 255; value += 1) {
     writeEEPROM(value + 512, digits[(value / 100) % 10]); //hundreds's place
   }
   Serial.println("Programming sign");
-  for (int value = 0; value < 255; value += 1) {
+  for (int value = 0; value <= 255; value += 1) {
     writeEEPROM(value + 768, 0); //first 7 segment
   }
+
+  Serial.println("Programming ones place (twos complement)");
+  for (int value = -128; value <= 127; value += 1) {
+    writeEEPROM((byte)value + 1024, digits[abs(value) % 10]);
+  }
+  Serial.println("Programming tens place (twos complement)");
+  for (int value = -128; value <= 127; value += 1) {
+    writeEEPROM((byte)value + 1280, digits[abs(value / 10) % 10]);
+  }
+  Serial.println("Programming hundreds place (twos complement)");
+  for (int value = -128; value <= 127; value += 1) {
+    writeEEPROM((byte)value + 1536, digits[abs(value / 100) % 10]);
+  }
+  Serial.println("Programming sign (twos complement)");
+  for (int value = -128; value <= 127; value += 1) {
+    if (value < 0) {
+      writeEEPROM((byte)value + 1792, 0x01); //g in 7 segment
+    }
+    else {
+      writeEEPROM((byte)value + 1792, 0);
+    }
+  }
+
   Serial.println("Reading EEPROM");
   printContents();
 }
